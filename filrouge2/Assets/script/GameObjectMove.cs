94187796e30c7d9642c62c4b912ac8ffffb6373c
momae93 <31903617+momae93@users.noreply.github.com>
2018-02-06 22:59:28 +0100

    [ADD] Moving platform behavior
    Add moving platform behavior
    Working on factory design pattern for the platform generation

---
+++ filrouge2/Assets/script/GameObjectMove.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameObjectMove : MonoBehaviour {
+	public int speed = 2;
+	public int leftLimit = -10;
+	public int rightLimit = 10;
+
+	private Vector3 nextPosition;
+	private Vector3 begin;
+	private Vector3 end;
+
+	void Start () {
+		begin = new Vector3 (leftLimit, transform.position.y);
+		end = new Vector3 (rightLimit, transform.position.y);
+		nextPosition = begin;
+	}
+	
+	void Update () {
+		Move ();	
+	}
+
+	void Move()
+	{
+		transform.position = Vector3.MoveTowards(transform.position, nextPosition, speed * Time.deltaTime);
+		if (Vector3.Distance (transform.position, nextPosition) <= 0.1)
+			nextPosition = nextPosition == begin ? end : begin;
+	}
+}
--- filrouge2/Assets/script/PlatformManager.cs
+++ filrouge2/Assets/script/PlatformManager.cs
@@ -3,26 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlatformManager : MonoBehaviour {
-
-    // Use this for initialization
+	
     public GameObject[] BasicPlatforms;
     public GameObject player;
+
     private float spawnY = -2f;
     private float platformHeight = 2.0f;
     private int platformOnScreen = 4;
     private Transform playerTransform;
-
     private GameObject lastPlatform;
 
 
 	void Start () {
         playerTransform = player.transform;
-		Spawn (Random.Range(-6,6), spawnY);
+		Spawn (Random.Range(-10,10), spawnY);
         for (int i = 0; i < platformOnScreen; i++)
             SpawnPlatForm();
 	}
 
-    // Update is called once per frame
     void Update () {
         if (playerTransform.position.y > spawnY - platformOnScreen * platformHeight)
             SpawnPlatForm();
@@ -34,7 +32,7 @@ public class PlatformManager : MonoBehaviour {
 		float rightPos = lastPlatform.transform.position.x + lastPlatform.transform.localScale.x / 2;
 		float[] tab = new float[] {	rightPos + 2f,	leftPos - 2f };
 		if (leftPos + 10 < 3f && 10 - (rightPos) < 3f)
-			Spawn(tab[Random.Range(0,1)], spawnY);
+			Spawn(tab[Random.Range(0,2)], spawnY);
 		else if (leftPos + 10 < 3f)
 			Spawn(tab[0], spawnY);
 		else
@@ -44,8 +42,17 @@ public class PlatformManager : MonoBehaviour {
 	private void Spawn(float x, float y, float z = 0)
 	{
 		GameObject newPlatform = Instantiate(BasicPlatforms[Random.Range(0, 2)]) as GameObject;
+		if (Random.Range (0, 2) == 1)
+			SetMovingBehavior (ref newPlatform);
 		newPlatform.transform.position = new Vector3(x, y, z);
 		lastPlatform = newPlatform;
 		spawnY += 2f;
+	}
+
+	private void SetMovingBehavior(ref GameObject gameObject)
+	{
+		gameObject.AddComponent<GameObjectMove> ();
+		gameObject.GetComponent<GameObjectMove> ().leftLimit = -5;
+		gameObject.GetComponent<GameObjectMove> ().rightLimit = 5;
 	}
 }
