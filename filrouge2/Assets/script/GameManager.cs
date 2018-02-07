71b86d9bbfc32006ebcefa64c842217ec849f9a3
momae93 <31903617+momae93@users.noreply.github.com>
2018-02-08 00:40:07 +0100

    [MODIFY] Pause script + add GameManager

---
+++ filrouge2/Assets/script/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    private float[] bearings;
+
+    public float score;
+    public int level;
+    public float waveSpeed;
+    public GameObject player;
+    public float scoreMax;
+	// Use this for initialization
+	void Start () {
+        score = 0;
+        level = 0;
+        waveSpeed = 2f;
+        bearings = new float[] {75, 175, 300, 500 };
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        UpdateScore();
+    }
+
+    void UpdateScore()
+    {
+        float currentY = player.transform.position.y;
+        if (currentY > scoreMax)
+        {
+            scoreMax = currentY;
+            CheckLevel(currentY);
+        }
+    }
+    void CheckLevel(float playerHeight)
+    {
+        if (scoreMax > bearings[level] && level < 3)
+        {
+            level += 1;
+            Debug.Log("Difficulty up :" + level);
+        }
+    }
+}
