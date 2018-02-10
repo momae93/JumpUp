33c306bd5f069704463ce16c20de114534259977
momae93 <31903617+momae93@users.noreply.github.com>
2018-02-10 11:59:11 +0100

    [CAMERA] Add offset field
    Add offset field so the player has a further vertical vision

--- filrouge2/Assets/script/CameraController.cs
+++ filrouge2/Assets/script/CameraController.cs
@@ -5,16 +5,16 @@ using UnityEngine;
 public class CameraController : MonoBehaviour {
 
     public GameObject player;
+    public float offset = 3f;
     public float speed = 2.0f;
 
     void Start()
-    {
-    }
+    { }
 
     void LateUpdate()
     {
         Vector3 v3 = transform.position;
-        v3.y = Mathf.Lerp(v3.y, player.transform.position.y, Time.deltaTime * speed);
+        v3.y = Mathf.Lerp(v3.y, player.transform.position.y + offset, Time.deltaTime * speed);
         transform.position = v3;
     }
 }
