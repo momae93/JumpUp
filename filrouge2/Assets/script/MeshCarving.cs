ec985223832b85c879a685a77442725e97ad8dba
momae93 <31903617+momae93@users.noreply.github.com>
2018-02-11 01:55:09 +0100

    [BACKGROUND] Added infinite background

---
+++ filrouge2/Assets/script/MeshCarving.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshCarving : MonoBehaviour {
+
+    // Use this for initialization
+    public GameObject ToCarve;
+	void Start () {
+	}
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKey(KeyCode.X))
+            Carve();
+    }
+
+    void Carve()
+    {
+        MeshFilter mf = ToCarve.GetComponent<MeshFilter>();
+        Debug.Log("Mesh size : " + mf.mesh.triangles.Length);
+    }
+}
